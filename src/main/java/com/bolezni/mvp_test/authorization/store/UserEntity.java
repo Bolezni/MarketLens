3951b43ebf183d24fcb76c5b09b12ba1f86aed42
Bolezni <Yurchenkods05@gmail.com>
2026-03-15 15:04:36 +0300

    init

---
+++ src/main/java/com/bolezni/mvp_test/authorization/store/UserEntity.java
@@ -0,0 +1,36 @@
+package com.bolezni.mvp_test.authorization.store;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "_users")
+@Entity
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String plan;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
---
+++ src/test/java/com/bolezni/mvp_test/MvpTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolezni.mvp_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
