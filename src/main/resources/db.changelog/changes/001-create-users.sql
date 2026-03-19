afd9afaf941e996f9747ecc6f6af27eb7c0b7f89
Bolezni <Yurchenkods05@gmail.com>
2026-03-19 18:41:49 +0300

    update liquebase conf

--- src/main/resources/db.changelog/changes/001-create-users.sql
+++
@@ -1,7 +0,0 @@
-create table _users{
-    id VARCHAR primary key,
-    email VARCHAR(255) unique not null,
-    password varchar(255) not null,
-    plan varchar(255),
-    created_at TIMESTAMP DEFAULT NOW()
-}
